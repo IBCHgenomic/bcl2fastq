d3f033af44d1a2bcc29261984666d6efd05befc5
IBCHgenomic <gsablok@ibch.poznan.pl>
2025-04-28 12:33:37 +0200

    update bcl2fastq and adding the webpage

--- bcl2fastq.ts
+++ bcl2fastq.ts
@@ -1,8 +1,27 @@
-
 /*
  Author Gaurav Sablok
  Instytut Chemii Bioorganicznej
  Polskiej Akademii Nauk
  ul. Noskowskiego 12/14 | 61-704, Poznań
- Date: 2025-4-22
+ Date: 2025-4-28
 */
+
+/*
+ typescript illumina interface
+*/
+
+interface Illumina {
+          sampleid: String,
+          samplename: String,
+          sampleplate: String,
+          samplewell: String,
+          indexplate: String,
+          index1id: String,
+          index: String,
+          index2id: String,
+          index2: String,
+          sampleproject: String,
+          description: String,
+}
+
+var interface: n
